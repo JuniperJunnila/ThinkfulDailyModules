33bc4af1a2e9833f8bbc22afffef59d1d3c592a9
Juniper <juneconsolidated@gmail.com>
2021-09-13 09:29:43 -0700

    Began Data Structures and Algorithms

---
+++ M41_INTRODUCTION_TO_DATA_STRUCTURES_AND_ALGORITHMS/C4/src/cable.js
@@ -0,0 +1,19 @@
+function cable1(currentLength, requiredLength, saleLength) {
+  let count = 0;
+  let i = currentLength;
+  while (i < requiredLength) {
+    i += saleLength;
+    count++;
+  }
+  return count;
+}
+
+function cable2(currentLength, requiredLength, saleLength) {
+  let distance = requiredLength - currentLength;
+  return Math.round(distance / saleLength);
+}
+
+module.exports = {
+  cable1,
+  cable2,
+};
